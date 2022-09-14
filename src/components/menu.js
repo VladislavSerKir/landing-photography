9b06be45b3eca73f0f4df5c381e81fb210c909fc
Vladislav Kireev <kireew.vladislaw@gmail.com>
2022-09-14 19:57:00 +0300

    hotfix: solving closing menu after clicking links

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,28 +1,38 @@
 const headerElement = document.querySelector('.header');
 const menuButton = headerElement.querySelector('.header__bar');
 const headerLogo = headerElement.querySelector('.header__logo');
+const headerLinks = headerElement.querySelectorAll('.header__link');
 const headerMenu = headerElement.querySelector('.header__links');
 const closeButton = headerElement.querySelector('.header__close-button');
 const wrapper = headerElement.querySelector('.wrapper');
 const headerContainer = headerElement.querySelector('.header__container');
 
-// разворачивание бургерного меню на мобильном разрешении
-menuButton.addEventListener("click", () => {
+function toggleMenu() {
     closeButton.classList.toggle("header__close-button_visible");
     headerContainer.toggleAttribute("hidden");
     wrapper.toggleAttribute("hidden");
+}
+
+// разворачивание бургерного меню на мобильном разрешении
+menuButton.addEventListener("click", () => {
+    toggleMenu();
 });
 
 closeButton.addEventListener("click", () => {
-    closeButton.classList.toggle("header__close-button_visible");
-    headerContainer.toggleAttribute("hidden");
-    wrapper.toggleAttribute("hidden");
+    toggleMenu();
 });
+
+headerLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (!wrapper.hidden) {
+            toggleMenu()
+        }
+    });
+})
 
 window.addEventListener('resize', function (event) {
     if (event.target.innerWidth > 768 && Array.from(closeButton.classList).includes('header__close-button_visible')) {
-        closeButton.classList.toggle("header__close-button_visible");
-        headerContainer.toggleAttribute("hidden");
-        wrapper.toggleAttribute("hidden");
+        toggleMenu();
     }
 }, true);
+
